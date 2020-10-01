21486239b7b9f6539999b7fb6f27ba0bd5678304
Alejandra Cordero <alejandra.corderogarcia21@gmail.com>
2020-10-01 22:56:07 +0200

    -Detail in script

--- build-microservices.sh
+++ build-microservices.sh
@@ -7,6 +7,7 @@ cd ../
 cd city-service/
 cd config && export $(cat config.env | xargs) && cd ../
 ./mvnw clean install
+cd ../
 cd gateway/
 cd config && export $(cat config.env | xargs) && cd ../
 ./mvnw clean install
