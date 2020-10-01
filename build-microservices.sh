56a31d398e9ffb4c59cd89ac1dd2590b64a6d12b
Alejandra Cordero <alejandra.corderogarcia21@gmail.com>
2020-10-01 19:10:57 +0200

    -Added scripts to run microservices

---
+++ build-microservices.sh
@@ -0,0 +1,13 @@
+cd itinerary-service/
+./mvnw clean install
+cd ../
+cd discovery-server/
+./mvnw clean install
+cd ../
+cd city-service/
+cd config && export $(cat config.env | xargs) && cd ../
+./mvnw clean install
+cd gateway/
+cd config && export $(cat config.env | xargs) && cd ../
+./mvnw clean install
+cd ../
